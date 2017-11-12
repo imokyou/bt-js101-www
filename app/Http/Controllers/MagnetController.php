6f0fab5b493a3bbfd67d16282a8f82b887e6308c
望月 <lupt059@163.com>
2017-11-12 20:52:28 +0800

    fix bug

--- app/Http/Controllers/MagnetController.php
+++ app/Http/Controllers/MagnetController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+
 use App\Magnet;
 
 class MagnetController extends Controller
@@ -21,9 +23,23 @@ class MagnetController extends Controller
         if(!empty($name)) {
             $collects = $magnet->where('name', 'like', "%{$name}%")->take(100)->orderBy('_id', 'desc')->get();
         } else {
-            $collects = $magnet->take(100)->orderBy('_id', 'desc')->get();
+
+            $collects = Cache::get('collects', '');
+            if(empty($collects)) {
+                $collects = $magnet->take(100)->orderBy('_id', 'desc')->get();
+                Cache::set('collects', json_encode($collects), 1);
+            }else {
+                $collects = json_decode($collects);
+            }
+            
         }
-        $total = $magnet->count();
+
+        $total = Cache::get('total', '');
+        if($total === '') {
+            $total = $magnet->count();
+            Cache::set('total', $total, 1);
+        }
+        
         return view('magnet.list', compact('collects', 'name', 'total'));
     }
 }
